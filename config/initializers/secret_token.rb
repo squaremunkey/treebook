a03fd4b7d08eeee714162f6a0f17ab4faf1a66f3
squaremunkey <info@squaremunkey.com>
2013-05-16 13:07:10 +0200

    created rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '97fc0203718253a1e09d673aee696cce3bd18362eeba2d2c073ec43d7d04427e8197e5495d20fae1ffec41987906bb69fe1d15d8788eee8dac1b64355fce8586'
